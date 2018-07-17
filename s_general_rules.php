3006db8f9d240fb1adbd96b6e71db00d1fff4021
Tyler Junek <tyler.junek@outlook.com>
2018-07-16 18:10:25 -0600

    Added more rules, footer.php, header.php.

--- s_general_rules.php
+++ s_general_rules.php
@@ -1,83 +1,48 @@
 <!DOCTYPE html> 
     <html lang="en-US">
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    </head>
-
-    <body>
-        <img source="images/serverlogo.png">
-        <h1> General Rules </h1>
-        
-        <div class="dropdown">
-				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"> Featured
-				<span class="caret"></span></button>
-				<ul class="dropdown-menu">
-						<li><a href="#">General Rules</a></li>
-						<li><a href="#">Community Board</a></li>
-				</ul>
-			</div>
-
-			<div class="dropdown">
-				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"> Info
-				<span class="caret"></span></button>
-				<ul class="dropdown-menu">
-						<li><a href="#">Rules</a></li>
-						<li><a href="#">Staff</a></li>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Become a Member!</a></li>
-				</ul>
-			</div>
-			
-			<div class="dropdown">
-				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"> Minecraft
-				<span class="caret"></span></button>
-				<ul class="dropdown-menu">
-						<li><a href="#">Servers</a></li>
-						<li><a href="#">Rules</a></li>
-						<li><a href="#">Tips & Tricks</a></li>
-				</ul>
-            </div>
-            
+    <link rel="stylesheet" href="s_styles.css">
+    <?php require_once('header.php') ?>
+    <?php require_once('footer.php') ?>
+        <h1> General Rules </h2>
             <h2> Community and Staff </h2>
-            <ul class="rules">
-                <li> Be respectful to everyone. This may be a server made for fighting, but have some honor and
-                respect. Everytones here to have a good time, including you!</li>
-                <li> Be respectful to staff and moderators, especially since they do have the power to ban you from
-                servers or even the site. They're there to make sure everything is running properly and everyong is having
-                fun, not to harrass and annoy when you're bored. </li>
-                <li> Speaking of which, if you caught breaking rules you may be punished by suspension or even
-                ban. Suspensions are temporary, bans are forever. All staff have the authority to issue punishments to
-                a certain extent and have the authority to stop you to talk to you and ask questions. If you disobey lower 
-                authority staff, punishments may become more severe.  </li>
-                <li> In any case where you feel as if a staff member is treating you unfairly or is being direspectful,
-                please report them to another staff member and they will take the steps neccesary to see that the
-                incident is looked into. If they do not, continue to report until you grab another staffs attention.</li>
-                <li> On the same note,  if you feel as if you have been unfairly punshed you can ask a staff to get
-                a manager to review it when they have the time. However, bans and suspensions would be hard to reverse as
-                staff should only issue those punishments when they are sure its necessary. </li>
-                <li> Staff are there to help, but they may not always be readily available or able to assist you at 
-                moments notice, so please be patient with them and try to avoid spamming them as it could result in
-                a punishment. They're tyring their best and will get to you eventually! :) </li>
-                <li> With all these rules around the staff and their role, authority, and what they do here, please
-                do not be afraid to conversate and talk with staff as all staff should be friendly and polite, mostly because
-                they have to reason not to be! If they aren't, you may report them as mentioned before.Staff should
-                not be seen as someone to be afraid of but more as someone who keeps the server at its best.</li>
-            </ul>
+                <ul class="rules">
+                    <li> Be respectful to everyone. This may be a server made for fighting, but have some honor and
+                    respect. Everytones here to have a good time, including you!</li>
+                    <li> Be respectful to staff and moderators, especially since they do have the power to ban you from
+                    servers or even the site. They're there to make sure everything is running properly and everyong is having
+                    fun, not to harrass and annoy when you're bored. </li>
+                    <li> Speaking of which, if you caught breaking rules you may be punished by suspension or even
+                    ban. Suspensions are temporary, bans are forever. All staff have the authority to issue punishments to
+                    a certain extent and have the authority to stop you to talk to you and ask questions. If you disobey lower 
+                    authority staff, punishments may become more severe.  </li>
+                    <li> In any case where you feel as if a staff member is treating you unfairly or is being direspectful,
+                    please report them to another staff member and they will take the steps neccesary to see that the
+                    incident is looked into. If they do not, continue to report until you grab another staffs attention.</li>
+                    <li> On the same note,  if you feel as if you have been unfairly punshed you can ask a staff to get
+                    a manager to review it when they have the time. However, bans and suspensions would be hard to reverse as
+                    staff should only issue those punishments when they are sure its necessary. </li>
+                    <li> Staff are there to help, but they may not always be readily available or able to assist you at 
+                    moments notice, so please be patient with them and try to avoid spamming them as it could result in
+                    a punishment. They're tyring their best and will get to you eventually! :) </li>
+                    <li> With all these rules around the staff and their role, authority, and what they do here, please
+                    do not be afraid to conversate and talk with staff as all staff should be friendly and polite, mostly because
+                    they have to reason not to be! If they aren't, you may report them as mentioned before.Staff should
+                    not be seen as someone to be afraid of but more as someone who keeps the server at its best.</li>
+                </ul>
 
             <h2> Servers and Cheating </h2>
-            <ul class="rules">
-                <li> To make this very simple, CHEATING, MODDING, HACKING, OR ANYTHING THAT GIVES YOU AN ADVANTAGE
-                OVER OTHER PLAYERS WILL RESULT IN IMMIDIATE BANS. We are making this a very strict 0 tolerance
-                poicy. </li>
-                <li> If you believe someone is cheating, please report them to staff and they will look into it
-                further. If the staff memeber decides that player is cheating he will take proper action, 
-                if not he will let you know and will not persue any further. You may not report someone after
-                a staff member has looked into them more than once for you. 
-                <li> You may be banned from seperate servers, a game server in general (for example you may not
-                be able to play Minecraft on any of our servers), or from the site which in turn would mean from all
-                game servers. If you are banned, the staff member you are dealing with should make your ban is clear
-                to you and why you are being banned. If you are suspended they should tell you for how long and why. </li>
-                <li> By playing on our servers, you are agreeing that you have not only read but will 
+                <ul class="rules">
+                    <li> To make this very simple, CHEATING, MODDING, HACKING, OR ANYTHING THAT GIVES YOU AN ADVANTAGE
+                    OVER OTHER PLAYERS WILL RESULT IN IMMIDIATE BANS. We are making this a very strict 0 tolerance
+                    poicy. </li>
+                    <li> If you believe someone is cheating, please report them to staff and they will look into it
+                    further. If the staff memeber decides that player is cheating he will take proper action, 
+                    if not he will let you know and will not persue any further. You may not report someone after
+                    a staff member has looked into them more than once for you. 
+                    <li> You may be banned from seperate servers, a game server in general (for example you may not
+                    be able to play Minecraft on any of our servers), or from the site which in turn would mean from all
+                    game servers. If you are banned, the staff member you are dealing with should make your ban is clear
+                    to you and why you are being banned. If you are suspended they should tell you for how long and why. </li>
+                    <li> By playing on our servers, you are agreeing that you have not only read but will follow all rules
+                    for the site and whatever servers you play on. If you do not follow the rules you may be punished. </li> 
+                    <li> 
--- s_index.php
+++ s_index.php
@@ -1,50 +1,9 @@
 <!DOCTYPE html>
     <html>
-    <link rel="stylesheet" href="s_styles.css">
-        <head>
-			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		</head>
-
-        <body>
+	<link rel="stylesheet" href="s_styles.css">
+	
 			<img src="images/VideoLogo.png">
             <h1> Welcome to Simplic! The Perfect PvP experience. </h1>
-			
-			<div class="dropdown">
-				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"> Featured
-				<span class="caret"></span></button>
-				<ul class="dropdown-menu">
-						<li><a href="#">HTML</a></li>
-						<li><a href="#">CSS</a></li>
-						<li><a href="#">JavaScript</a></li>
-				</ul>
-			</div>
-
-			<div class="dropdown">
-				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"> Info
-				<span class="caret"></span></button>
-				<ul class="dropdown-menu">
-						<li><a href="#">Rules</a></li>
-						<li><a href="#">Staff</a></li>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Become a Member!</a></li>
-				</ul>
-			</div>
-			
-			<div class="dropdown">
-				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"> Minecraft
-				<span class="caret"></span></button>
-				<ul class="dropdown-menu">
-						<li><a href="#">Servers</a></li>
-						<li><a href="#">Rules</a></li>
-						<li><a href="#">Tips & Tricks</a></li>
-				</ul>
-			</div>
-
 			<p> Welcome to Simplic! Add description here. </p>
-			
-			
-        </body>
+        
     </html>
