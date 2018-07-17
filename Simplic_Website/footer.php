5e838b406b5a23e3a7a5d0e2d1d5c501a82565fc
Tyler Junek <tyler.junek@outlook.com>
2018-07-16 21:23:30 -0600

    Added footer.php and made minor changes across index and styles.

---
+++ Simplic_Website/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+            <footer>
+                <div class="container">
+                    <nav class="navbar-fixed-bottom navbar-inverse">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="index.php"> Simplic </a>
+                        </div>
+                        <ul class="navbar navbar-nav">
+                            <li class="active"> <a href="#"> # </a> </li>
+                            <li> <a href="#"> # </a> <li>
+                            <li> <a href="#"> # </a> <li>
+                            <li> <a href="#"> # </a> <li>
+                    </nav>
+                </div>
+            </footer>
+        </body>
+    </html>
