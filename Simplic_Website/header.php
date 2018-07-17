184344b5fe5dbdc0f73e59b3a1810a11d1282eac
CommonSenze <commonsenze00@gmail.com>
2018-07-16 22:26:12 -0500

    Fixing Header

--- Simplic_Website/header.php
+++ Simplic_Website/header.php
@@ -1,5 +1,39 @@
 <!DOCTYPE html>
-<html>
-    <html lang="en-US">
-    <link rel="stylesheet" href="s_styles.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+<html lang="en-US">
+    <head>
+        <link rel="stylesheet" href="s_styles.css">
+        <title>Simplic</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-static-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Simplic</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+                <script type="text/javascript">
+                    function validateSearch() {
+                        var a = document.forms["playerSearch"]["search"].value;
+                        if (a==null || a=="") {
+                            alert("Please enter a value in the field.");
+                            return false;
+                        }
+                    }
+                </script>
+                <form class="navbar-form navbar-left" name="playerSearch" onsubmit="return validateSearch()" action="#">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search" name="search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+            </div>
+        </nav>
--- Simplic_Website/index.php
+++ Simplic_Website/index.php
@@ -3,6 +3,7 @@
         <p> Simplic is a server meant to allow players to have an Practice crossed Early HG environment in a respectful,
         friendly atmosphere. The server is custom coded and able to flip on the dime to allow players to have instant bug
         fixes and useful perks they believe will be better for the server. Suggestions, comments, and ideas are always welcome.
-        We hope you can be apart of this fantastic community and feel welcome! </p> 
+        We hope you can be apart of this fantastic community and feel welcome! </p>
+
 
         
