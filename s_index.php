4578519ee72ce915d64f2e00e2238604f99f3f82
CommonSenze <commonsenze00@gmail.com>
2018-07-16 19:31:26 -0500

    Fresh start
    Fresh start

--- s_index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-    <html>
-	<link rel="stylesheet" href="s_styles.css">
-	
-			<img src="images/VideoLogo.png">
-            <h1> Welcome to Simplic! The Perfect PvP experience. </h1>
-			<p> Welcome to Simplic! Add description here. </p>
-        
-    </html>
