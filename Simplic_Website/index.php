13d1f1d663cb09f1b3c9cb237fb0cde94a473712
CommonSenze <commonsenze00@gmail.com>
2018-07-16 22:48:04 -0500

    Update

--- Simplic_Website/index.php
+++ Simplic_Website/index.php
@@ -4,6 +4,5 @@
         friendly atmosphere. The server is custom coded and able to flip on the dime to allow players to have instant bug
         fixes and useful perks they believe will be better for the server. Suggestions, comments, and ideas are always welcome.
         We hope you can be apart of this fantastic community and feel welcome! </p>
-
-
-        
+<img src="img/minecraft_screenshot1.png" class="homepage_img">
+<?php require_once('footer.php') ?>        
