06523cd6df005494c4773506004ea797d2136d27
Tyler Junek <tyler.junek@outlook.com>
2018-07-16 21:58:28 -0600

    Footer fix again

--- Simplic_Website/footer.php
+++ Simplic_Website/footer.php
@@ -3,11 +3,12 @@
                         <div class="navbar-header">
                             <a class="navbar-brand" href="index.php"> Simplic </a>
                         </div>
-                        <ul class="navbar navbar-nav">
+                        <ul class="navbar navbar-nav ulft">
                             <li class="active"> <a href="#"> # </a> </li>
                             <li> <a href="#"> # </a> <li>
                             <li> <a href="#"> # </a> <li>
                             <li> <a href="#"> # </a> <li>
+                        </ul>
                     </nav>
             </footer>
         </body>
